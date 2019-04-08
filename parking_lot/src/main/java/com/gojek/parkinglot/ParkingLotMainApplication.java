02ba9e707bcbbf887312559962856878e3242217
akhan58 <akhan58@jcp.com>
2019-04-08 12:14:53 +0530

    Adding gradle module with main application

---
+++ parking_lot/src/main/java/com/gojek/parkinglot/ParkingLotMainApplication.java
@@ -0,0 +1,17 @@
+package com.gojek.parkinglot;
+
+import java.io.FileNotFoundException;
+
+/**
+ * Created by azeem.khan on 8/April/19.
+ */
+public class ParkingLotMainApplication {
+
+    public ParkingLotMainApplication(String[] args) throws FileNotFoundException {
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+    }
+}
