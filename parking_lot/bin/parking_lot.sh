13e0e4ee2ff11866e580f8242c60c5f7f7ba24b4
akhan58 <akhan58@jcp.com>
2019-04-08 21:18:45 +0530

    Fixing parking lot sh file

--- parking_lot/bin/parking_lot.sh
+++ parking_lot/bin/parking_lot.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env sh
 
 main(){
-	CurrentDir=( `pwd` )
-	( cd ${CurrentDir}/../ && gradle clean build )
+    echo "=========Gradle build========="
+    build $*
+    echo "Build done====="
+
+    CUR_DIR=( `pwd`)
+    ARG=$1
+    LIBS=${CUR_DIR}/../build/install/parking_lot/lib
+    ( java -cp .:${LIBS}/* com.gojek.parkinglot.ParkingLotMainApplication ${ARG} )
+    echo "Completed"
+}
+
+build(){
+    CurrentDir=( `pwd` )
+	( cd ${CurrentDir}/../ &&  gradle clean distZip distTar installDist )
 }
 main $*
