bc87e907b4fc7a88e316c03939ddd2d89af6fae3
akhan58 <akhan58@jcp.com>
2019-04-08 14:42:47 +0530

    updating main method to process the parking lot application

--- parking_lot/src/main/java/com/gojek/parkinglot/ParkingLotMainApplication.java
+++ parking_lot/src/main/java/com/gojek/parkinglot/ParkingLotMainApplication.java
@@ -1,17 +1,28 @@
 package com.gojek.parkinglot;
 
+import com.gojek.parkinglot.reader.InputReader;
+import com.gojek.parkinglot.reader.InputReaderFactory;
+import com.gojek.parkinglot.services.ParkingLotService;
+import com.gojek.parkinglot.services.impl.ParkingLotServiceImpl;
+
 import java.io.FileNotFoundException;
 
 /**
  * Created by azeem.khan on 8/April/19.
  */
 public class ParkingLotMainApplication {
+    private InputReaderFactory inputReaderFactory;
+    private InputReader inputReader;
+    private ParkingLotService parkingLotService;
 
     public ParkingLotMainApplication(String[] args) throws FileNotFoundException {
-
+        inputReaderFactory = new InputReaderFactory();
+        parkingLotService = new ParkingLotServiceImpl();
+        inputReader = inputReaderFactory.build(args, parkingLotService);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-
+        ParkingLotMainApplication parkingLotApplication = new ParkingLotMainApplication(args);
+        parkingLotApplication.inputReader.processInput();
     }
 }
