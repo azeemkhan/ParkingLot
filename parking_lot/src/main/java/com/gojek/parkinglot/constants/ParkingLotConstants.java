881f9149dbeeb95696b0429a425953716f7381d1
akhan58 <akhan58@jcp.com>
2019-04-08 14:42:08 +0530

    changes in constant message

--- parking_lot/src/main/java/com/gojek/parkinglot/constants/ParkingLotConstants.java
+++ parking_lot/src/main/java/com/gojek/parkinglot/constants/ParkingLotConstants.java
@@ -1,4 +1,13 @@
 package com.gojek.parkinglot.constants;
 
-public enum ParkingLotConstants {
+/**
+ * Created by azeem.khan on 8/April/19.
+ */
+public class ParkingLotConstants {
+    public static final String PARKING_LOT_CREATED_MSG = "Created a parking lot with %s slots";
+    public static final String PARKING_ALLOTTED_MSG = "Allocated slot number: %s";
+    public static final String PARKING_LEAVE_MSG = "Slot number %s is free";
+    public static final String PARKING_FULL_MSG = "Sorry, parking lot is full";
+    public static final String NOT_FOUND_MSG = "Not found";
+    public static final String PARKING_LOT_NOT_CREATED_MSG = "Parking Lot has not been created yet";
 }
